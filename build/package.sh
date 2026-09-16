615b1e7bf2355b38582f6232fd8417f8c29a7fcb
Pepe Barrascout <pepebarrascout@users.noreply.github.com>
2026-09-16 01:58:50 +0000

    v0.0.0.4 (sin alpha): versioning compatible con Jellyfin
    Jellyfin no acepta versiones con sufijos como 'alpha'. Cambio a version
    numerica pura: 0.0.0.4
    
    - meta.json: version 0.0.0.4
    - manifest.json: version 0.0.0.4, sourceUrl actualizado
    - csproj: AssemblyVersion/FileVersion/Version 0.0.0.4
    - build/package.sh: VERSION=0.0.0.4
    - Eliminado ZIP del alpha

--- build/package.sh
+++ build/package.sh
@@ -7,7 +7,7 @@ set -e
 PROJECT_DIR="/home/z/my-project/jellyfin-plugin-youtube"
 BIN_DIR="$PROJECT_DIR/Jellyfin.Plugin.YouTube/bin/Release/net9.0"
 DIST_DIR="$PROJECT_DIR/dist"
-VERSION="0.0.0.4-alpha"
+VERSION="0.0.0.4"
 
 # DLLs that Jellyfin already provides - DO NOT include in the zip
 # These include all transitive dependencies of Jellyfin.Controller/Model/Extensions:
